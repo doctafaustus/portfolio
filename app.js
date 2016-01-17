b7f5aa495004e662abec067dcadfac8bacd3f117
doctafaustus <clowd150@gmail.com>
2016-01-17 13:49:19 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+// Globals
+var port = process.env.PORT || 3000;
+
+
+// Initiate Express
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.listen(port, function(req, res) {
+	console.log('App listening on port 3000');
+});
+
+
+// Routes
+app.get('/', function(req, res) {
+	res.render('index.ejs');
+});
